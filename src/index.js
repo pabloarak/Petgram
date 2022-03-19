3cef2615d15088ad41e336d9b5ced4580d64628f
Pablo Arak <pablo.arak.vm@gmail.com>
2022-03-19 12:38:12 -0300

    Render App

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 import ReactDOM from 'react-dom'
+import { App } from './App'
 
-ReactDOM.render(<h1>Hola</h1>, document.getElementById('app'))
+ReactDOM.render(<App />, document.getElementById('app'))
