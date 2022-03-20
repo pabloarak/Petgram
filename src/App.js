ab23c36f4d8181ecd2cb16a102b1558e7d8c3d69
Pablo Arak <pablo.arak.vm@gmail.com>
2022-03-20 13:59:50 -0300

    Added first animations

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { ListOfCategories } from './components/ListOfCategories'
-import { GlobalStyle } from './GlobalStyles'
+import { GlobalStyle } from './styles/GlobalStyles'
 import { ListOfPhotoCards } from './components/ListOfPhotoCards'
 import { Logo } from './components/Logo'
 
