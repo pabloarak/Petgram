f23758b8e4c4f14bb70b9a11c30e5f6a95880ee8
Pablo Arak <pablo.arak.vm@gmail.com>
2022-03-20 17:01:36 -0300

    Added position fixed when scroll is greater than 200

--- src/components/ListOfCategories/index.js
+++ src/components/ListOfCategories/index.js
@@ -1,15 +1,46 @@
+import { useEffect, useState, Fragment } from 'react'
 import { Category } from '../Category'
 import { List, Item } from './styles'
-import data from '../../../api/db.json'
 
 export const ListOfCategories = () => {
-  return (
-    <List>
-      {data.categories.map((category) => (
+  const [categories, setCategories] = useState([])
+  const [showFixed, setShowFixed] = useState(false)
+
+  useEffect(() => {
+    window
+      .fetch('https://petgram-server-pabloarak.vercel.app/categories')
+      .then((res) => res.json())
+      .then((response) => {
+        setCategories(response)
+      })
+  }, [])
+
+  useEffect(() => {
+    const onScroll = (e) => {
+      const newShowFixed = window.scrollY > 200
+
+      showFixed !== newShowFixed && setShowFixed(newShowFixed)
+    }
+
+    document.addEventListener('scroll', onScroll)
+
+    return () => document.removeEventListener('scroll', onScroll)
+  }, [showFixed])
+
+  const renderList = (fixed) => (
+    <List className={fixed ? 'fixed' : ''}>
+      {categories.map((category) => (
         <Item key={category.id}>
           <Category {...category} />
         </Item>
       ))}
     </List>
+  )
+
+  return (
+    <>
+      {renderList()}
+      {showFixed && renderList(showFixed)}
+    </>
   )
 }
