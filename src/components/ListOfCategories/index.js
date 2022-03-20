29bd32283bb312d86c22dfe600929bc04259a32a
Pablo Arak <pablo.arak.vm@gmail.com>
2022-03-20 11:45:48 -0300

    Added json database with categories

--- src/components/ListOfCategories/index.js
+++ src/components/ListOfCategories/index.js
@@ -1,12 +1,13 @@
 import { Category } from '../Category'
 import { List, Item } from './styles'
+import data from '../../../api/db.json'
 
 export const ListOfCategories = () => {
   return (
     <List>
-      {[1, 2, 3, 4].map((category) => (
-        <Item key={category}>
-          <Category />
+      {data.categories.map((category) => (
+        <Item key={category.id}>
+          <Category {...category} />
         </Item>
       ))}
     </List>
