e60769e61310f919b5f2e56b55ffd9eb86f24d1d
Pablo Arak <pablo.arak.vm@gmail.com>
2022-03-20 13:24:12 -0300

    Added logo

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import { ListOfCategories } from './components/ListOfCategories'
 import { GlobalStyle } from './GlobalStyles'
 import { ListOfPhotoCards } from './components/ListOfPhotoCards'
+import { Logo } from './components/Logo'
 
 export const App = () => (
   <div>
     <GlobalStyle />
+    <Logo />
     <ListOfCategories />
     <ListOfPhotoCards />
   </div>
