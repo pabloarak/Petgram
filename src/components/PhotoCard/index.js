b5998ab831b949603deff61b9d269d12fc33bcc1
Pablo Arak <pablo.arak.vm@gmail.com>
2022-03-20 22:41:47 -0300

    Added import only is required

--- src/components/PhotoCard/index.js
+++ src/components/PhotoCard/index.js
@@ -1,40 +1,45 @@
 import { Article, ImgWrapper, Img, Button } from './styles'
 import { MdFavoriteBorder } from 'react-icons/md'
 import { useEffect, useRef, useState } from 'react'
-const DEFAULT_IMAGE = 'https://res.cloudinary.com/midudev/image/upload/w_300/q_80/v1560262103/dogs.png'
+const DEFAULT_IMAGE =
+  'https://res.cloudinary.com/midudev/image/upload/w_300/q_80/v1560262103/dogs.png'
 
 export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE }) => {
   const domElement = useRef(null)
   const [show, setShow] = useState(false)
 
   useEffect(() => {
-    const observer = new window.IntersectionObserver((entries) => {
-      const { isIntersecting } = entries[0] // Nos ayuda a saber si está en el viewport
-      if (isIntersecting) {
-        setShow(true)
-        observer.disconnect()
-      }
+    Promise.resolve(
+      typeof window.IntersectionObserver !== 'undefined'
+        ? window.IntersectionObserver
+        : import('intersection-observer')
+    ).then(() => {
+      const observer = new window.IntersectionObserver((entries) => {
+        const { isIntersecting } = entries[0] // Nos ayuda a saber si está en el viewport
+        if (isIntersecting) {
+          setShow(true)
+          observer.disconnect()
+        }
+      })
+      observer.observe(domElement.current)
     })
-    observer.observe(domElement.current)
   }, [domElement])
 
   return (
     <Article ref={domElement}>
-      {
-        show &&
-          <>
-            <a href={`/detail/${id}`}>
-              <ImgWrapper>
-                <Img src={src} />
-              </ImgWrapper>
-            </a>
+      {show && (
+        <>
+          <a href={`/detail/${id}`}>
+            <ImgWrapper>
+              <Img src={src} />
+            </ImgWrapper>
+          </a>
 
-            <Button>
-              <MdFavoriteBorder size='32px' /> {likes} likes !
-            </Button>
-          </>
-      }
-
+          <Button>
+            <MdFavoriteBorder size='32px' /> {likes} likes !
+          </Button>
+        </>
+      )}
     </Article>
   )
 }
